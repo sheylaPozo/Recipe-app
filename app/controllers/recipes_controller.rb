9066a812b8ee09edd343cce523366249b0dd1e3b
m05e5 <calebmoses1111@gmail.com>
2021-11-23 15:10:37 +0100

    add recipe list, my recipe list and delete recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,19 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    @recipes = Recipe.all.order(updated_at: :desc)
+  end
 
   def show; end
 
-  def my_recipes; end
+  def my_recipes
+    @recipes = Recipe.where(user_id: current_user.id).order(updated_at: :desc)
+  end
 
   def create; end
 
-  def destroy; end
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to(request.env['HTTP_REFERER'], notice: 'recipe deleted succesfully')
+  end
 end
