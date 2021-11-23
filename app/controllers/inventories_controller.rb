c996cfd69e009b81986388d39efd81e05a2a9508
eroiyo <eroiyo@outlook.es>
2021-11-23 10:50:10 -0400

    displaying users inventories information

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,5 +1,7 @@
 class InventoriesController < ApplicationController
   def index
+    @current_user = current_user
+    @inventories = Inventory.where(user_id: @current_user.id)
   end
 
   def show
