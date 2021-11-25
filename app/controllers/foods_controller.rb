cef2c12f30f3ea5d38ef67fc0da27caecae66cf8
m05e5 <calebmoses1111@gmail.com>
2021-11-25 20:02:28 +0100

    add food list and add food form

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 
 class FoodsController < ApplicationController
-  def index; end
+  def index
+    @foods = Food.all.order(created_at: :desc)
+    @current_user = current_user
+    @measurement = [['kg', 'kg'], ['gramme', 'gramme'], ['unit', 'unit']]
+  end
 
-  def create; end
+  def create
+    @food = Food.new
+    @food.user_id = current_user.id
+    @food.name = params[:name]
+    @food.measurement_unit = params[:measurement_unit]
+    @food.price = params[:price]
+    redirect_to(request.env['HTTP_REFERER'], notice: 'food created succesfully') if @food.save
+  end
 
-  def destroy; end
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to(request.env['HTTP_REFERER'], notice: 'food deleted succesfully')
+  end
 end
